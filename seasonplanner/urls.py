3c564d224db3840da3f7e2b4eed8d2bd4ee35358
Scott Graham <yobttocs@hotmail.com>
2013-05-17 21:13:12 -0500

    Changed the urls in the templates to use the url tag

--- seasonplanner/urls.py
+++ seasonplanner/urls.py
@@ -7,11 +7,11 @@ from seasonplanner import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^seasons/$', views.seasons, name='seasons'),
-    url(r'^seasons/(?P<pk>\d+)$',
+    url(r'^seasons/(?P<pk>\d+)/$',
         views.SeasonDetail.as_view(model=Season, template_name='seasonplanner/detail.html'),
         name='detail'),
     url(r'^seasons/create/$', views.create, name='create'),
-    url(r'^weeks/(?P<pk>\d+)$',
+    url(r'^weeks/(?P<pk>\d+)/$',
         DetailView.as_view(model=Week, template_name='seasonplanner/week_detail.html'),
         name='week_detail'),
 )
