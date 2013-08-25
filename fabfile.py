00fb057177830a67a8c4078c93c551e4fb3c981a
Scott Graham <yobttocs@hotmail.com>
2013-08-24 21:52:15 -0500

    Cleaned up some python modules that weren't being used because of version discrepancies and added the requirements.txt file

--- fabfile.py
+++
@@ -1,7 +0,0 @@
-from fabric.api import local
-
-def prepare_push(branch_name):
-    local('python manage.py test seasonplanner')
-#    local('git add -p && git commit')
-#    local('git push origin')
-    
